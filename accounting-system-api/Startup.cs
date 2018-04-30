47abdcf7b6d62e5932825d8f1b775aca840bc0ec
Alejandro Matute <alexghoro@outlook.com>
2018-04-29 23:15:29 -0400

    Add project files.

---
+++ accounting-system-api/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccountingSystemApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccountingSystemApi
+{
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("myDB"));
+
+      services.AddMvc().AddJsonOptions(ConfigureJson);
+    }
+
+    private void ConfigureJson(MvcJsonOptions obj)
+    {
+      obj.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext myContext)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseMvc();
+
+
+      if (!myContext.Paises.Any())
+      {
+        myContext.Paises.AddRange(new List<Pais>()
+        {
+          new Pais()
+          {
+            Nombre = "República Dominicana",
+            Provincias = new List<Provincia>()
+            {
+              new Provincia(){Nombre = "Azua"}
+            }
+          },
+          new Pais()
+          {
+            Nombre = "México",
+            Provincias = new List<Provincia>()
+            {
+              new Provincia(){Nombre = "Puebla"},
+              new Provincia(){Nombre = "Queretaro"}
+            }
+          },
+          new Pais()
+          {
+            Nombre = "Argentina"
+          }
+        });
+
+        myContext.SaveChanges();
+      }
+
+
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("MVC did't find  anything");
+      });
+      
+    }
+  }
+}
